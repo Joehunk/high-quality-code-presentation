3cdf66a5e3c28a2cc40f8ecdcad1399476a52098
Charles Crain <craincha@amazon.com>
2021-01-13 15:21:03 -0600

    Added another end to end test

--- __tests__/cli_system.test.ts
+++ __tests__/cli_system.test.ts
@@ -14,3 +14,16 @@ test("end to end", async () => {
   await runCommandLineInterpreter(underTest);
   expect(writer.readOutput()).toBe("> hello\n> Exiting.\n");
 });
+
+test("end to end 2", async () => {
+  const reader = createInputFromLines('echo "hello    world"', "exit");
+  const writer = createOutputCapture();
+  const underTest = createCliSystem({
+    input: reader,
+    output: writer,
+    prompt: "?",
+  });
+
+  await runCommandLineInterpreter(underTest);
+  expect(writer.readOutput()).toBe("? hello    world\n? Exiting.\n");
+});
