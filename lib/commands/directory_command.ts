ae2bac5b6b2555b4525b2c7441a3fb5ffaa0adbf
Charles Crain <ccrain@iconbuild.com>
2022-03-26 12:37:33 -0500

    Revert to language native approach

--- lib/commands/directory_command.ts
+++ lib/commands/directory_command.ts
@@ -1,8 +1,5 @@
 import * as F from "fp-ts/function";
 import * as RA from "fp-ts/ReadonlyArray";
-import * as E from "fp-ts/Either";
-import * as TE from "fp-ts/TaskEither";
-import * as T from "fp-ts/Task";
 import { HasFileSystem, HasOutput } from "../cli_system";
 import { Result, SingleCommandProcessor } from "./command_model";
 
@@ -29,48 +26,20 @@ async function ensureAllDirectoriesExist(directories: string[], dependencies: Ha
   }
 }
 
-// Normally we would write a unit test for this too.
-export async function pipeGeneratorToSink<T>(
-  gen: AsyncGenerator<T, unknown, unknown>,
-  func: (arg: T) => Promise<unknown>
-): Promise<void> {
-  for await (const value of gen) {
-    await func(value);
-  }
-}
-
 export const directoryProcessor: SingleCommandProcessor<HasOutput & HasFileSystem> = {
   command: "ls",
   async process(args: string[], dependencies: HasOutput & HasFileSystem): Promise<Result> {
-    const task = F.pipe(
-      TE.of(args),
-      TE.filterOrElse(
-        // Notably I got this wrong initially (was "> 1"). This demonstrates that even for
-        // a function with branch complexity 1, "correct on sight" is not really achievable.
-        // Therefore you must simulate effects in an integration test here. So this approach
-        // appears to add extra complexity for not much extra value over the language
-        // native approach.
-        //
-        // Another possible lesson learned here is that a helper like "argsNotEmpty" might
-        // have made this code more readable.
-        (args) => args.length >= 1,
-        () => Result.continue("Must supply at least one directory.")
-      ),
-      TE.chainFirst((args) => TE.fromTask(() => ensureAllDirectoriesExist(args, dependencies))),
-      TE.chain((args) =>
-        F.pipe(
-          args,
-          TE.traverseSeqArray((arg) =>
-            TE.fromTask(() =>
-              pipeGeneratorToSink(dependencies.fileSystem.listFilesInDirectory(arg), dependencies.output.writeLine)
-            )
-          )
-        )
-      ),
-      TE.map(F.constant(Result.continue())),
-      TE.getOrElse(T.of)
-    );
+    if (args.length < 1) {
+      return Result.continue("Must supply at least one directory.");
+    }
 
-    return await task();
+    await ensureAllDirectoriesExist(args, dependencies);
+
+    for (const directory of args) {
+      for await (const file of dependencies.fileSystem.listFilesInDirectory(directory)) {
+        await dependencies.output.writeLine(file);
+      }
+    }
+    return Result.continue();
   },
 };
