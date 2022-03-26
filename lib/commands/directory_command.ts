46d9271ec67e647e6c01b3f3cef215c05bbcae7f
Charles Crain <ccrain@iconbuild.com>
2022-03-26 12:33:48 -0500

    Power through with fp-ts just to see what it's like.
    Going to revert back to language native after this.
    
    Lessons learned:
    1. fp-ts is great but when your compositions get longer than ~5 lines it becomes hard to read.
    2. Branch complexity = 1 does not imply correct-on-sight. I had a > vs >= error that integration
    tests needed to catch.
    3. Overall the language native approach is less overall code and is more readable.
    4. However using fp-ts in targeted ways like the concatErrors() function still seems like
    a win.

--- lib/commands/directory_command.ts
+++ lib/commands/directory_command.ts
@@ -1,5 +1,8 @@
 import * as F from "fp-ts/function";
 import * as RA from "fp-ts/ReadonlyArray";
+import * as E from "fp-ts/Either";
+import * as TE from "fp-ts/TaskEither";
+import * as T from "fp-ts/Task";
 import { HasFileSystem, HasOutput } from "../cli_system";
 import { Result, SingleCommandProcessor } from "./command_model";
 
@@ -26,20 +29,48 @@ async function ensureAllDirectoriesExist(directories: string[], dependencies: Ha
   }
 }
 
+// Normally we would write a unit test for this too.
+export async function pipeGeneratorToSink<T>(
+  gen: AsyncGenerator<T, unknown, unknown>,
+  func: (arg: T) => Promise<unknown>
+): Promise<void> {
+  for await (const value of gen) {
+    await func(value);
+  }
+}
+
 export const directoryProcessor: SingleCommandProcessor<HasOutput & HasFileSystem> = {
   command: "ls",
   async process(args: string[], dependencies: HasOutput & HasFileSystem): Promise<Result> {
-    if (args.length < 1) {
-      return Result.continue("Must supply at least one directory.");
-    }
+    const task = F.pipe(
+      TE.of(args),
+      TE.filterOrElse(
+        // Notably I got this wrong initially (was "> 1"). This demonstrates that even for
+        // a function with branch complexity 1, "correct on sight" is not really achievable.
+        // Therefore you must simulate effects in an integration test here. So this approach
+        // appears to add extra complexity for not much extra value over the language
+        // native approach.
+        //
+        // Another possible lesson learned here is that a helper like "argsNotEmpty" might
+        // have made this code more readable.
+        (args) => args.length >= 1,
+        () => Result.continue("Must supply at least one directory.")
+      ),
+      TE.chainFirst((args) => TE.fromTask(() => ensureAllDirectoriesExist(args, dependencies))),
+      TE.chain((args) =>
+        F.pipe(
+          args,
+          TE.traverseSeqArray((arg) =>
+            TE.fromTask(() =>
+              pipeGeneratorToSink(dependencies.fileSystem.listFilesInDirectory(arg), dependencies.output.writeLine)
+            )
+          )
+        )
+      ),
+      TE.map(F.constant(Result.continue())),
+      TE.getOrElse(T.of)
+    );
 
-    await ensureAllDirectoriesExist(args, dependencies);
-
-    for (const directory of args) {
-      for await (const file of dependencies.fileSystem.listFilesInDirectory(directory)) {
-        await dependencies.output.writeLine(file);
-      }
-    }
-    return Result.continue();
+    return await task();
   },
 };
